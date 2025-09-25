8438bc6da3c55f8551b4ca8a3987ddbffefb78a2
Arindam Saha <arindamsaha1507@gmail.com>
2025-09-25 11:33:31 +0200

    Return the tuples

--- square.py
+++ square.py
@@ -1,26 +1,41 @@
 
-original_number = 2
-
-
-def square(number):
-
-    return number**2
-
 def pythagorean_triplets(limit):
 
     x = 0
     y = 0
+
+    results = []
 
     for z in range(1, limit + 1):
 
         for x in range(1, limit + 1):
             for y in range(1, limit +1):
                 if x**2 + y**2 == z**2:
-                    print(x, y, z)
+                    results.append([x, y, z])
+
+    return results
+
+"""
+
+Future topics
+1. Tuples vs List
+
+Input -> Nothing
+
+1. initialize
+2. Loops
+3. Loops
+4. Loops
+5. Checks
+6. Prints
+"""
 
 def main():
     limit = 20
-    pythagorean_triplets(limit)
+    triplets = pythagorean_triplets(limit)
+
+    print(triplets)
+
 
 
 if __name__ == "__main__":

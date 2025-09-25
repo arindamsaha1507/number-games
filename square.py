cd05009bb4f5d3f511ea16949293999a6f79d832
Arindam Saha <arindamsaha1507@gmail.com>
2025-09-25 11:13:45 +0200

    Working pythagorean triplets

--- square.py
+++ square.py
@@ -6,9 +6,21 @@ def square(number):
 
     return number**2
 
+def pythagorean_triplets(limit):
+
+    x = 0
+    y = 0
+
+    for z in range(1, limit + 1):
+
+        for x in range(1, limit + 1):
+            for y in range(1, limit +1):
+                if x**2 + y**2 == z**2:
+                    print(x, y, z)
 
 def main():
-    print(square(original_number))
+    limit = 20
+    pythagorean_triplets(limit)
 
 
 if __name__ == "__main__":
